4ceab01a2006efba8b99469527090080c1e9582e
Antonio <rssntn67@yahoo.it>
2023-08-21 14:23:04 +0200

    Created src files from spring boot ini

---
+++ src/main/java/com/example/restservicecomplete/RestServiceCompleteApplication.java
@@ -0,0 +1,13 @@
+package com.example.restservicecomplete;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestServiceCompleteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestServiceCompleteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/restservicecomplete/RestServiceCompleteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restservicecomplete;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestServiceCompleteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
